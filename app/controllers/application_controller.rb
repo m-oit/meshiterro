c95914e9d8b6c330dc51e7d2314a2c0f63220a37
umay.oit07@gmail.com <177226299+m-oit@users.noreply.github.com>
2024-09-25 02:54:18 +0000

    [finish]meshiterro 6章

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #↑divise利用の機能(ユーザ登録、ログイン認証etcが使われる前に
+  #configure_permitted_parametersメソッドが実行される)
+  #このメソッドはdvise_parameter_sanitizer.permitを使うことでサインアップの際にユーザー名のデータ操作を許可している
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
