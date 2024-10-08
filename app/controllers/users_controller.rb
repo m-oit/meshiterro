bba873e4c083d423e98c292f2f5ce4cf48faeaa0
EC2 Default User <177226299+m-oit@users.noreply.github.com>
2024-10-08 02:58:35 +0000

    [Finish]他のユーザーからのアクセスを制限しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])  
   end
 
   def edit
-    @user = User.find(params[:id])
+   @user = User.find(params[:id])
+   
   end
   
   def update
@@ -16,9 +19,14 @@ class UsersController < ApplicationController
   end
   
   private
-  
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
   
+    def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
 end
